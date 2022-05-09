05c7202cda2530bbf675d08a1f7094598c4d1a46
Catchthembodies <103537060+Catchthembodies@users.noreply.github.com>
2022-05-08 22:08:17 -0700

    commit tree 1f0b9fd1b41fc07970ea945cc12c5ded9727dbf7

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+var newHeader = document.createElement('h1');
+newHeader.setAttribute('id', "victory");
+newHeader.innerHTML = "Ikmann is the champion";
+
+document.body.appendChild(newHeader);
